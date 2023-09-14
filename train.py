0ac698be0037b414b083c7cc78c1204fa4dd74eb
Raja Haseeb <rajahaseeb147@gmail.com>
2023-09-14 13:46:21 +0900

    Update train.py

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ import os
 import random
 import time
 from datetime import datetime
+from test import *
 
 import numpy as np
 import torch
@@ -18,23 +19,21 @@ from clearml import Logger, Task
 from torch.optim.lr_scheduler import MultiStepLR, StepLR
 from torch.utils.data import DataLoader, Subset, random_split
 
-from config import Config
+from config import Config, logger
 from data import Dataset
 from models import *
 from utils import compute_model_complexity
 
-from lightning.fabric import Fabric
-from lightning.fabric.strategies import DDPStrategy
+import torch.multiprocessing as mp
+from torch.nn import DataParallel
 
 random.seed(17)
 torch.manual_seed(17)
 
-
 # Save model utility
-def save_model(state, save_path, name, epoch, fabric):
-    save_name = os.path.join(save_path, name + "_" + str(epoch) + ".ckpt")
-    fabric.save(save_name, state)
-
+def save_model(model, save_path, name, epoch):
+    save_name = os.path.join(save_path, name + "_" + str(epoch) + ".pth")
+    torch.save(model.state_dict(), save_name)
 
 # Sanity check subset function
 def create_subset(trainset, valset):
@@ -44,23 +43,15 @@ def create_subset(trainset, valset):
     return train_subset, val_subset
 
 
-if __name__ == "__main__":
+def main():
 
     # * Read args
+    logger.info("Reading arguments...")
     args = Config()
-
-    # * Fabric setup
-    if args.use_gpu:
-        ddp = DDPStrategy(process_group_backend="gloo")
-        fabric = Fabric(accelerator="cuda", devices=2, strategy=ddp, num_nodes=1)
-    else:
-        fabric = Fabric(accelerator="cpu")
-    fabric.launch()
-    fabric.seed_everything(17)
+    logger.info(args)
 
     # * Load data
-    fabric.print("-" * 100)
-    fabric.print("Preparing dataset...")
+    logger.info("Preparing dataset...")
     dataset = Dataset(
         args.train_root,
         args.train_list,
@@ -80,22 +71,20 @@ if __name__ == "__main__":
         batch_size=args.train_batch_size,
         shuffle=True,
         num_workers=args.num_workers,
+        pin_memory=True
     )
     val_loader = DataLoader(
-        val_set, batch_size=args.train_batch_size, num_workers=args.num_workers
+        val_set, batch_size=args.train_batch_size, num_workers=args.num_workers, pin_memory=True
     )
 
-    fabric.print(f"Total training images: {len(train_set)}")
-    fabric.print(f"Total validation images: {len(val_set)}")
+    logger.info(f"Total training images: {len(train_set)}")
+    logger.info(f"Total validation images: {len(val_set)}")
 
     # identity_list = get_lfw_list(args.lfw_test_list)
     # img_paths = [os.path.join(args.lfw_root, each) for each in identity_list]
 
     # * Training setup
     # Define model
-    fabric.print("-" * 100)
-    fabric.print("Creating model...")
-
     if args.backbone == "resnet18":
         model = resnet_face18(use_se=args.use_se)
     elif args.backbone == "resnet34":
@@ -107,12 +96,12 @@ if __name__ == "__main__":
     elif args.backbone == "slimnet":
         model = SlimNet()
     elif args.backbone == "model-timm":
-        model = create_model("mobilenetv3_small_050")
+        model = create_model("mobilenetv3_small_050", pretrained=True)
 
-    fabric.print(f"Using '{args.backbone}' model...")
+    logger.info(f"Using {args.backbone} model...")
     params, flops = compute_model_complexity(model, (1, 3, 128, 128))
-    fabric.print("Total params: %.2fM" % (params / 1000000.0))
-    fabric.print("Total flops: %.2fM" % (flops / 1000000.0))
+    print("Total params: %.2fM" % (params / 1000000.0))
+    print("Total flops: %.2fM" % (flops / 1000000.0))
 
     # Loss, metric and optimizer
     if args.loss == "focal_loss":
@@ -155,59 +144,63 @@ if __name__ == "__main__":
             weight_decay=args.weight_decay,
         )
 
+    
+    # Set device
+    if args.use_multi:
+        model = DataParallel(model)
+        metric_fc = DataParallel(metric_fc)
+    if args.use_gpu:
+        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        model.to(device)
+        metric_fc.to(device)
+
     if args.scheduler == "multistep":
         scheduler = MultiStepLR(optimizer, milestones=args.milestones, gamma=args.gamma)
     else:
         scheduler = StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
 
-    model, optimizer = fabric.setup(model, optimizer)
-    metric_fc = fabric.to_device(metric_fc)
-    train_loader, val_loader = fabric.setup_dataloaders(train_loader, val_loader)
-    fabric.print("-" * 100)
-    fabric.print("Fabric Initialized...")
 
     # * Create clearml task
-    fabric.print("-" * 100)
-    fabric.print("Initializing ClearML Task...")
+    logger.info("Initializing ClearML Task...")
     task = Task.init(
         project_name="3i-Facereid",
-        task_name=f"resnet18-webface-{datetime.now()}",
+        task_name=f"{args.backbone}-webface-128x128-{datetime.now()}",
         # output_uri="/home/jovyan/haseeb-rnd/haseeb-data/artifacts/facereid/",
     )
     task.connect(args, name="args")  # add args to clearml logging
 
     # * Training loop
-    best_val_loss = np.inf
+    best_val_loss = float("inf")
     epochs_without_improvement = 0
-    fabric.print("-" * 100)
-    fabric.print(f"Starting training...")
+    logger.info(f"Starting training...")
     start = time.perf_counter()
 
-    train_loss = np.inf
-    val_loss = np.inf
+    train_loss = float("inf")
+    val_loss = float("inf")
 
     for i in range(1, args.max_epochs + 1):
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        fabric.print(f"date and time = {dt_string}")
+        logger.info(f"date and time = {dt_string}")
         epoch_start = time.perf_counter()
-        fabric.print(f"learning rate: {optimizer.param_groups[0]['lr']}")
+        logger.info(f"learning rate: {optimizer.param_groups[0]['lr']}")
         model.train()
         epoch_loss = 0.0
         epoch_acc = 0.0
         total_iters = len(train_loader)
         for ii, data in enumerate(train_loader):
             data_input, label = data
-            label = label.long()
+            data_input = data_input.to(device)
+            label = label.to(device).long()
             feature = model(data_input)
             output = metric_fc(feature, label)
             loss = criterion(output, label)
             epoch_loss += loss.item()
             optimizer.zero_grad()
-            fabric.backward(loss)
+            loss.backward()
             optimizer.step()
 
-            fabric.print(
+            print(
                 f"Epoch [{i}/{args.max_epochs}] - Iteration [{ii+1}/{total_iters}] - Batch loss: {loss:.4f}"
             )
 
@@ -226,17 +219,16 @@ if __name__ == "__main__":
         train_acc = (epoch_acc / len(train_loader)) * 100
         train_loss = epoch_loss / len(train_loader)
 
-        Logger.current_logger().report_scalar("train", "loss", iteration=i, value=train_loss)
-        Logger.current_logger().report_scalar("train", "acc", iteration=i, value=train_acc)
-
-        fabric.print(
+        print(
             f"Epoch [{i}/{args.max_epochs}] - Train Loss: {train_loss:.4f} - Train Accuracy: {train_acc:.4f} - Epoch Time: {epoch_end-epoch_start:.4f}"
         )
 
+        Logger.current_logger().report_scalar("train", "loss", iteration=i, value=train_loss)
+        Logger.current_logger().report_scalar("train", "acc", iteration=i, value=train_acc)
+
         # * Validation step
         if i % args.val_interval == 0:
-            fabric.print("\n")
-            fabric.print("Running validation step...")
+            logger.info("Running validation step...")
             val_start = time.perf_counter()
             model.eval()
             epoch_loss = 0.0
@@ -244,7 +236,8 @@ if __name__ == "__main__":
             with torch.no_grad():
                 for ii, data in enumerate(val_loader):
                     data_input, label = data
-                    label = label.long()
+                    data_input = data_input.to(device)
+                    label = label.to(device).long()
                     feature = model(data_input)
                     output = metric_fc(feature, label)
                     loss = criterion(output, label)
@@ -259,30 +252,25 @@ if __name__ == "__main__":
                     # batch_acc = correct_predictions / len(label)
                     epoch_acc += batch_acc
 
-            val_end = time.perf_counter()
+                val_end = time.perf_counter()
 
-            # Calculate and log validation metrics
-            val_acc = (epoch_acc / len(val_loader)) * 100
-            val_loss = epoch_loss / len(val_loader)
+                # Calculate and log validation metrics
+                val_acc = (epoch_acc / len(val_loader)) * 100
+                val_loss = epoch_loss / len(val_loader)
 
-            fabric.print(
+            print(
                 f"Epoch [{i}/{args.max_epochs}] - Validation Loss: {val_loss:.4f} - Validation Accuracy: {val_acc:.4f} - Validation Time: {val_end-val_start:.4f}"
             )
 
-            Logger.current_logger().report_scalar("val", "loss", iteration=i, value=val_loss)   
+            Logger.current_logger().report_scalar("val", "loss", iteration=i, value=val_loss)
             Logger.current_logger().report_scalar("val", "acc", iteration=i, value=val_acc)
-
-            fabric.print("-" * 100)
 
             # Save model
             if val_loss + args.min_delta < best_val_loss:
-                state = {
-                    "model": model,
-                    "optimizer": optimizer,
-                    "iteration": i,
-                }
-                save_model(state, args.checkpoints_path, args.backbone, i, fabric)
-                fabric.print(f"Epoch [{i}] - Saved model successfully...")
+                if not os.path.exists(args.checkpoints_path):
+                    os.makedirs(args.checkpoints_path)
+                save_model(model, args.checkpoints_path, args.backbone, i)
+                logger.info(f"Epoch [{i}] - Saved model successfully...")
                 best_val_loss = val_loss
                 epochs_without_improvement = 0
             else:
@@ -290,12 +278,16 @@ if __name__ == "__main__":
 
             # Check early stopping criteria
             if epochs_without_improvement >= args.patience:
-                fabric.print(f"Early stopping at epoch {i}")
+                logger.info(f"Early stopping at epoch {i}")
                 break
 
-        scheduler.step()        
+        scheduler.step()
+        torch.cuda.empty_cache()
 
     # End of training
     end = time.perf_counter()
-    print("Training completed!")
+    logger.info("Training completed!")
     print(f"Total training time: {end - start}")
+
+if __name__ == "__main__":
+    main()
